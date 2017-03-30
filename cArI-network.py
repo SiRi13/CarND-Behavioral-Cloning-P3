b314f7661e500e509e14f53ec7c343f35f377018
Simon Ritzel <simon.ritzel@gmail.com>
2017-03-30 20:30:32 +0200

    first try with simple network

--- cArI-network.py
+++ cArI-network.py
@@ -19,26 +19,29 @@ with open('./data/driving_log.csv') as log:
 images = []
 measurements = []
 for line in lines:
+    if 'steering' in line:
+        continue
     # extract filename from path
     img_filename = line[0].split('/')[-1]
     image = cv2.imread(os.path.join('./data/IMG', img_filename))
+    steering_angle = float(line[3])
     images.append(image)
-    steering_angle = line[3]
-    if "," in steering_angle:
-        steering_angle = steering_angle.replace(',', '.')
-    measurements.append(float(steering_angle))
+    measurements.append(steering_angle)
+    images.append(cv2.flip(image, 1))
+    measurements.append(steering_angle * -1.0)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=1)
 
 model.save('cArI.h5')
---
+++ test.py
@@ -0,0 +1,4 @@
+import h5py
+
+f = h5py.File('./cArI.h5', mode='r')
+    
